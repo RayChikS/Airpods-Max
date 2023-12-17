c8fd22787dac045c627b9ff81f29e4d397178caf
RayChikS <vatoresp@gmail.com>
2023-12-17 14:30:33 +0300

    c

--- js/script.js
+++ js/script.js
@@ -1,43 +1,32 @@
 const chooseBtn = document.querySelectorAll(".choose-list-btn");
 const contentItem = document.querySelectorAll(".content-images-item");
 
-
-
-chooseBtn.forEach(function(elem) {
-	elem.addEventListener('click', open)
-})
-
+chooseBtn.forEach(function (elem) {
+  elem.addEventListener("click", open);
+});
 
 function open(evt) {
-    const target = evt.currentTarget;
-    const button = target.dataset.button;
-    console.log('Button:', button);
+  const target = evt.currentTarget;
+  const button = target.dataset.button;
+  console.log("Button:", button);
 
-    const contentActive = document.querySelectorAll(`.${button}`);
-    console.log('Content Active:', contentActive);
+  const contentActive = document.querySelectorAll(`.${button}`);
+  console.log("Content Active:", contentActive);
 
-    chooseBtn.forEach(function (item) {
-        item.classList.remove('btn-active');
-    });
+  chooseBtn.forEach(function (item) {
+    item.classList.remove("btn-active");
+  });
 
-    target.classList.add('btn-active');
+  target.classList.add("btn-active");
 
-    contentItem.forEach(function (item) {
-        item.classList.remove('content-active');
-    });
+  contentItem.forEach(function (item) {
+    item.classList.remove("content-active");
+  });
 
-    // Добавляем класс content-active только нужным элементам
-    contentActive.forEach(function (item) {
-        item.classList.add('content-active');
-    });
+  // Добавляем класс content-active только нужным элементам
+  contentActive.forEach(function (item) {
+    item.classList.add("content-active");
+  });
 }
 
-
-
-
-
-
-
-
-
-
+//dd
